72f84c3cfc2aa1f11c2e45872171f8d82662d371
1oneRanger <tsangwingshing@sjtu.edu.cn>
2025-03-27 23:10:34 +0800

    Backup

--- Train/train.bat
+++ Train/train.bat
@@ -2,5 +2,5 @@
 @echo off  
 call conda activate ml  
 cd D:\Unity\Project\training_system\Train  
-mlagents-learn MjReacher.yaml --run-id MjReacher_20250327223853 --torch-device cuda:0  
+mlagents-learn MjReacher.yaml --run-id MjReacher_20250327230933 --torch-device cuda:0  
 pause  
