61c23af47235e4772348dd30f5ae0223ab27bb7d
1oneRanger <tsangwingshing@sjtu.edu.cn>
2025-04-15 16:17:57 +0800

    Backup

--- Assets/Scripts/UTrain/Runtime/Components/JibotActuator.cs
+++ Assets/Scripts/UTrain/Runtime/Components/JibotActuator.cs
@@ -8,50 +8,20 @@ using Mujoco;
 public class JibotActuator : ActuatorComponent {
     // Reference value for the overall ActionSpec of the combined IActuators associated with the component
     public override ActionSpec ActionSpec => new ActionSpec(numContinuousActions: 7);
-
+    
     // We need to add the actuator from the Editor
     [SerializeField]
-    private MjActuator actuator0;
+    private MjActuator[] actuators;
     
-    [SerializeField]
-    private MjActuator actuator1;
-    
-    [SerializeField]
-    private MjActuator actuator2;
-    
-    [SerializeField]
-    private MjActuator actuator3;
-    
-    [SerializeField]
-    private MjActuator actuator4;
-    
-    [SerializeField]
-    private MjActuator actuator5;
-    
-    [SerializeField]
-    private MjActuator actuator6;
-    
-    private ControlledMjActuator[]
-        controlledMjActuators = new ControlledMjActuator[7]; // Wrapper object that applies actions from the Agent to the MjActuator.
+    private ControlledMjActuator
+        controlledMjActuator; // Wrapper object that applies actions from the Agent to the MjActuator.
 
     public override IActuator[] CreateActuators() {
-        controlledMjActuators[0] = new ControlledMjActuator(actuator0, 0);
-        controlledMjActuators[1] = new ControlledMjActuator(actuator1, 1);
-        controlledMjActuators[2] = new ControlledMjActuator(actuator2, 2);
-        controlledMjActuators[3] = new ControlledMjActuator(actuator3, 3);
-        controlledMjActuators[4] = new ControlledMjActuator(actuator4, 4);
-        controlledMjActuators[5] = new ControlledMjActuator(actuator5, 5);
-        controlledMjActuators[6] = new ControlledMjActuator(actuator6, 6);
+        controlledMjActuator = new ControlledMjActuator(actuators);
         
         return new[]
         {
-            controlledMjActuators[0],
-            controlledMjActuators[1],
-            controlledMjActuators[2],
-            controlledMjActuators[3],
-            controlledMjActuators[4],
-            controlledMjActuators[5],
-            controlledMjActuators[6]
+            controlledMjActuator,
         }; // Could create and return multiple IActuators if needed
     }
 
@@ -62,10 +32,9 @@ public class JibotActuator : ActuatorComponent {
             get => actionSpec;
         }
 
-        private MjActuator wrappedActuator;
-
-        public int id;
-        public string Name => wrappedActuator.name;
+        private MjActuator[] wrappedActuators = new MjActuator[7];
+        
+        public string Name => wrappedActuators[0].name;
 
 
         // Used when no model is connected to the BrainParameters component.
@@ -75,7 +44,13 @@ public class JibotActuator : ActuatorComponent {
 
         // The agent distributes its actions among all of its IActuators, each receiving a segment based on their ActionSpecs.
         public void OnActionReceived(ActionBuffers actionBuffers) {
-            wrappedActuator.Control = actionBuffers.ContinuousActions[id];
+            wrappedActuators[0].Control = actionBuffers.ContinuousActions[0] * 20.0f;
+            wrappedActuators[1].Control = actionBuffers.ContinuousActions[1] * 20.0f;
+            wrappedActuators[2].Control = actionBuffers.ContinuousActions[2] * 20.0f;
+            wrappedActuators[3].Control = actionBuffers.ContinuousActions[3] * 20.0f;
+            wrappedActuators[4].Control = actionBuffers.ContinuousActions[4] * 20.0f;
+            wrappedActuators[5].Control = actionBuffers.ContinuousActions[5] * 20.0f;
+            wrappedActuators[6].Control = -actionBuffers.ContinuousActions[5] * 20.0f;
         }
 
 
@@ -89,10 +64,9 @@ public class JibotActuator : ActuatorComponent {
         }
 
 
-        public ControlledMjActuator(MjActuator actuator, int num) {
+        public ControlledMjActuator(MjActuator[] actuators) {
             actionSpec = new ActionSpec(numContinuousActions: 7);
-            wrappedActuator = actuator;
-            id = num;
+            wrappedActuators = actuators;
         }
 
     }
