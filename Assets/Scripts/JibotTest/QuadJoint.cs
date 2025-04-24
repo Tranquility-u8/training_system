2cb6ae3f41fc93ea5aad64b3a16393c382b56f4e
1oneRanger <tsangwingshing@sjtu.edu.cn>
2025-04-24 19:15:01 +0800

    Backup

---
+++ Assets/Scripts/JibotTest/QuadJoint.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadJoint : MonoBehaviour
+{
+    [SerializeField]
+    private Transform pa;
+    
+    [SerializeField]
+    private Transform pb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
